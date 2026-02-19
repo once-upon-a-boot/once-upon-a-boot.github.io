f23c392fa5ce16d99028e9d8b66959f11d405c6d
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2026-02-19 11:09:16 -0800

    url

--- traces.sh
+++ traces.sh
@@ -2,20 +2,30 @@
 
 set -euo pipefail
 
+if [ -z "${DISABLE_CONTAINER_CHECK:-}" ]; then
+    ./container.sh $0 "$@"
+    exit 0
+fi
+
 input()
 {
     cat uftrace.data/exec.log | cut -f 1 -d ' ' | grep '^[0-9]'
 }
 
 start=$(input | head -n 1)
-start=$(python -c "print(int($start))")
+start=$(python3 -c "print(int($start))")
 end=$(input | tail -n 1)
-end=$(python -c "print(int($end) + 1)")
+end=$(python3 -c "print(int($end) + 1)")
 
 rm -rf traces/
 mkdir -p traces/
 
 for ts in $(seq $start $end); do
     range=$(python3 -c "print(str($ts - 0.2) + '~' + str($ts + 1.2))")
-    printf "uftrace dump --chrome --srcline --time-range=$range | gzip -9 > traces/$ts.gz\n";
+    here="$(pwd)/"
+    url="https://github.com/once-upon-a-boot/code/blob/main/"
+    # srcline requires recent uftrace
+    printf "uftrace dump --chrome --srcline --time-range=$range | sed -e \"s#$here#$url#\" | gzip -9 > traces/$ts.gz\n";
 done | parallel -j $(nproc) --bar
+
+du -h traces/
