f13d587adf1e5fb034e01e987dccd8fe86dc61a1
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2026-02-19 11:59:41 -0800

    wip

--- index.sh
+++ index.sh
@@ -6,10 +6,13 @@ content()
 {
     cat ./exec.log | while read line; do
         ts=$(echo $line | cut -f 1 -d ' ')
+        ts_ns=$(echo $ts | sed -e 's/\.//')
         out=$(echo $line | cut -f 2- -d ' ')
         trace_name=$(echo $ts | cut -f 1 -d '.')
         trace_url=https://raw.githubusercontent.com/once-upon-a-boot/traces/main/$trace_name.gz
-        perfetto_url="perfetto/#!/?url=$trace_url"
+        start=$((ts_ns - 1000))
+        end=$((ts_ns + 1000))
+        perfetto_url="perfetto/#!/?url=$trace_url&visStart=$start&visEnd=$end"
         echo "<a href="$perfetto_url">🔍</a> $out"
     done
 }
