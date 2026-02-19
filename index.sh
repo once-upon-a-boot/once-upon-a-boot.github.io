68a35f9b5c2694902290185b42ecf6a66c22a8f6
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2026-02-19 11:53:18 -0800

    wip

--- index.sh
+++ index.sh
@@ -5,15 +5,19 @@ set -euo pipefail
 content()
 {
     cat ./exec.log | while read line; do
-    ts=$(cut -f 1 -d ' ')
-    out=$(cut -f 2- -d ' ')
-    trace_name=$(echo $ts | cut -f 1 -d '.')
-    echo $trace_name $out
+        ts=$(echo $line | cut -f 1 -d ' ')
+        out=$(echo $line | cut -f 2- -d ' ')
+        trace_name=$(echo $ts | cut -f 1 -d '.')
+        trace_url=https://raw.githubusercontent.com/once-upon-a-boot/traces/main/$trace_name.gz
+        perfetto_url="perfetto/#!/?url=$trace_url"
+        echo "<a href="$perfetto_url">🔍</a> $out"
     done
 }
 
 cat << EOF
 <body>
+<pre>
 $(content)
+</pre>
 </body>
 EOF
