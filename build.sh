3f8c00f46ad6f9f294268faa7003308e03f5c920
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2026-02-21 13:11:42 -0800

    wip

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 ./index.sh  > docs/index.html
+rsync -av ../perfetto/out/ui/ui/dist/ docs/perfetto/
+# set default timestamp view to Milliseconds
+sed -i -e 's/defaultValue: timeline_1.TimestampFormat.Timecode,/defaultValue: timeline_1.TimestampFormat.Milliseconds,/'  ./docs/perfetto/v*/frontend_bundle.js
